cbb857678b990aee349c3bfbf9ba9d6571421b97
nutv99 <nutv99@gmail.com>
2022-08-18 10:20:36 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-lnaatp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-lnaatp)
---
+++ src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component, VERSION } from '@angular/core';
+import { FormGroup, FormControl,FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms'; 
+import { HttpClient, HttpHeaders  } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  departmentData: string[] = ['A', 'B', 'C'];
+  categoryData: string[] = [];
+  groupData: string[] = [];
+
+  departmentName : string = '' ;
+  categoryName : string = '' ;
+  groupName : string = '' ;
+
+  constructor(private http: HttpClient) {}
+
+  name = 'Angular ' + VERSION.major;
+
+  searchCategory() {
+    //alert(this.departmentName)
+    this.categoryData = ['A1', 'A2'];
+    const url = 'https://lovetoshopmall.com/dataservice/category.php?departmentName='+this.departmentName;
+    this.http.get<any>(url).subscribe( response => {
+      console.log(response)
+
+    })
+  }
+
+  searchGroup() {
+    //alert(this.categoryName)
+    this.groupData = ['A1111', 'A2222'];
+    const url = 'https://lovetoshopmall.com/dataservice/category.php?departmentName='+this.departmentName;
+    this.http.post(url,this.categoryName).subscribe(response =>{
+      console.log(response)
+    })
+
+  }
+}
