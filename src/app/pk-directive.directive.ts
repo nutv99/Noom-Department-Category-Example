9f8f087bd9cc6b2ffcc92e5949d82eb4aacb5913
nutv99 <nutv99@gmail.com>
2022-08-20 08:31:54 +0700

    2 files modified

--- src/app/pk-directive.directive.ts
+++ src/app/pk-directive.directive.ts
@@ -5,14 +5,16 @@ import { HttpClient, HttpHeaders  } from '@angular/common/http';
   selector: '[appPkDirective]'
 })
 export class PkDirective {
-@Input() url : string;
+
+
+@Input() myurl = '';
 @Output() appPkDirective = new EventEmitter()
 
   constructor(private http:HttpClient,private elementRef: ElementRef) {
     
   }
   @HostListener('input',['$event']) loadDepartment(event:any) {    
-    console.log(this.url) ;
+    console.log('My URL ',this.myurl) ;
     const inputTextElement :HTMLInputElement = event.target ;  
     this.http.get<any>('https://www.anapioficeandfire.com/api/books?name='+ inputTextElement.value)
     .subscribe(response => this.appPkDirective.emit(response)) ;
