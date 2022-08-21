c86de0c92a2d22e320c3078bb5c072e78acb5ff7
nutv99 <nutv99@gmail.com>
2022-08-21 14:10:40 +0700

    4 files modified

--- src/app/item-form/item-form.component.ts
+++ src/app/item-form/item-form.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { MyHttpService} from '../pick.service' ;
+import { Subscription,Subject, switchMap, debounceTime,pipe } from 'rxjs';
+
+
+interface City {
+  name: string,
+  code: string,
+  inactive:any 
+}
 
 @Component({
   selector: 'app-item-form',
@@ -7,9 +16,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemFormComponent implements OnInit {
 value1:string = '';
-  constructor() { }
+subscription?: Subscription;
 
-  ngOnInit() {
+onSearchDataSwitchMap = new Subject<string>();
+
+
+constructor(private myservice:MyHttpService) {
+
+  this.onSearchDataSwitchMap.pipe(
+    debounceTime(100),
+    switchMap(searchText=>{
+    return this.myservice.getDatasUniverSal3(this.value1)
+  })).subscribe(value=>console.log(value));
+
+  
+    this.cities = [
+        {name: 'New York', code: 'NY',inactive: false},
+        {name: 'Rome', code: 'RM', inactive: true},
+        {name: 'London', code: 'LDN', inactive: false},
+        {name: 'Istanbul', code: 'IST', inactive: true},
+        {name: 'Paris', code: 'PRS', inactive: false}
+    ];
+
+
+
+ }
+
+ 
+
+ngOnInit() {
+
+  
+}
+
+cities: City[]; 
+inactive:any ;
+
+    selectedCity: City;
+    
+  onSearh() {
+    let pageno = '1';
+    if (this.subscription) {
+           console.log('UnSubscribe ' );
+           this.subscription.unsubscribe ();
+    }
+     this.subscription = this.myservice.getDatasUniverSal3(this.value1).subscribe(response =>{
+     console.log (response)
+    })
   }
+
+  onSearhSwitchMap() {
+    let pageno = '1';
+    this.onSearchDataSwitchMap.next(this.value1) ; 
+  }
+
+
 
 }
--- src/app/pick.service.ts
+++ src/app/pick.service.ts
@@ -5,8 +5,8 @@ import {
   debounceTime,
   distinctUntilChanged,
   switchMap,
-  tap
-} from "rxjs/operators";
+  tap,
+} from 'rxjs/operators';
 
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
@@ -17,7 +17,7 @@ import {
   HttpParams,
   HttpResponse,
 } from '@angular/common/http';
-import { User,UserInformation } from './user';
+import { User, UserInformation } from './user';
 
 class SearchItem {
   constructor(
@@ -29,77 +29,85 @@ class SearchItem {
   ) {}
 }
 
-@Injectable({  providedIn: 'root' })
+@Injectable({ providedIn: 'root' })
 export class MyHttpService {
+  apiRoot: string = 'https://itunes.apple.com/search';
 
-  apiRoot: string = "https://itunes.apple.com/search";
-
-  Response:any ; 
-  constructor(private http:HttpClient) { }
+  Response: any;
+  constructor(private http: HttpClient) {}
 
   public getDatas(): Observable<any> {
-  const url = 'https://reqres.in/api/users?page=1';
+    const url = 'https://reqres.in/api/users?page=1';
 
     return this.http.get<any>(url);
   }
 
-  public InsertDatas(dataPost:any): Observable<any> {
-
+  public InsertDatas(dataPost: any): Observable<any> {
     const url = 'https://reqres.in/api/users?page=1';
     let httpOptions = {
-     headers: new HttpHeaders({ 'Content-Type': 'application/json',
-      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1OTU0NzE5MjMsImlzcyI6Imh0dHBzOi8vbG9jYWxob3N0OjQ0MzQxIiwiYXVkIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NDQzNDEifQ.FdHX7Ycs_Z5zwAQkRD9iyVRPsgS42QqC8yQ9EKYnVC4' })
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1OTU0NzE5MjMsImlzcyI6Imh0dHBzOi8vbG9jYWxob3N0OjQ0MzQxIiwiYXVkIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NDQzNDEifQ.FdHX7Ycs_Z5zwAQkRD9iyVRPsgS42QqC8yQ9EKYnVC4',
+      }),
     };
- 
-     return this.http.post<any>(url,dataPost,httpOptions);
+
+    return this.http.post<any>(url, dataPost, httpOptions);
   }
 
-  public UpdateDatas(dataPost:any): Observable<any> {
+  public UpdateDatas(dataPost: any): Observable<any> {
     const url = 'https://reqres.in/api/users?page=1';
-    return this.http.patch<any>(url,dataPost);
+    return this.http.patch<any>(url, dataPost);
   }
 
-  public ReplaceeDatas(dataPost:any): Observable<any> {
+  public ReplaceeDatas(dataPost: any): Observable<any> {
     const url = 'https://reqres.in/api/users?page=1';
- 
-    return this.http.put<any>(url,dataPost);
+
+    return this.http.put<any>(url, dataPost);
   }
 
-  public DeleteDatas(dataPost:any): Observable<any> {
+  public DeleteDatas(dataPost: any): Observable<any> {
     const url = 'https://reqres.in/api/users?page=1';
- 
-    return this.http.delete<any>(url,dataPost);
+
+    return this.http.delete<any>(url, dataPost);
   }
 
-  public getDatasUniverSal(pagecode:string): Observable<any> {
+  public getDatasUniverSal(pagecode: string): Observable<any> {
     //const url = 'https://lovetoshopmall.com/dataservice/getDataUniverSal.php?pagecode='+ pagecode ;
-    const url = 'https://lovetoshopmall.com/dataservice/categoryTest.php?pagecode='+ pagecode ;
-    this.http.get<any>(url).subscribe((response:any)=>{
-        this.Response = response ;
-        console.log('From BAck',this.Response)
-        return this.Response ;
-     }
-    );       
-    return this.Response ; 
-   }
+    const url =
+      'https://lovetoshopmall.com/dataservice/categoryTest.php?pagecode=' +
+      pagecode;
+    this.http.get<any>(url).subscribe((response: any) => {
+      this.Response = response;
+      console.log('From BAck', this.Response);
+      return this.Response;
+    });
+    return this.Response;
+  }
 
-   public getDatasUniverSal2(pagecode:string): Observable<any> {
+  public getDatasUniverSal2(pagecode: string): Observable<any> {
     //const url = 'https://lovetoshopmall.com/dataservice/getDataUniverSal.php?pagecode='+ pagecode ;
-    const url = 'https://lovetoshopmall.com/dataservice/categoryTest.php?pagecode='+ pagecode ;
-    return this.http.get<any>(url) ;
+    const url =
+      'https://lovetoshopmall.com/dataservice/categoryTest.php?pagecode=' +
+      pagecode;
+    return this.http.get<any>(url);
+  }
 
-    
-   }
+  public getDatasUniverSal3(pagecode: string): Observable<any> {
+    const url =
+      'https://pokeapi.co/api/v2/pokemon/ditto?pagecode=' +
+      pagecode;
+    return this.http.get<any>(url);
+  }
 
-   search(term: string): Observable<SearchItem[]> {
-    this.apiRoot  = "https://itunes.apple.com/search";
-    alert(this.apiRoot) ;
+  search(term: string): Observable<SearchItem[]> {
+    this.apiRoot = 'https://itunes.apple.com/search';
+    alert(this.apiRoot);
     let apiURL = `${this.apiRoot}?term=${term}&media=music&limit=20`;
-    return this.http.get(apiURL)
-     .pipe(        
-      map((response:any) => {
+    return this.http.get(apiURL).pipe(
+      map((response: any) => {
         console.log(response);
-        return response.results.map(item => {
+        return response.results.map((item) => {
           return new SearchItem(
             item.trackName,
             item.artistName,
@@ -111,6 +119,4 @@ export class MyHttpService {
       })
     );
   }
-
-
 }
