1330373c225ea882509381ed2a5b9864f3e100bc
nutv99 <nutv99@gmail.com>
2022-08-23 19:16:16 +0700

    2 files modified

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup , Validators  } from '@angular/forms';
-import {AbstractControl } from '@angular/forms';
+
 import { MyHttpService } from '../pick.service';
-import {customer,customer_PK} from '../model' ;
+import { customer, customer_PK } from '../model';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 
 import { Subject, throwError } from 'rxjs';
 import { catchError, finalize, tap } from 'rxjs/operators';
-
-
 
 @Component({
   selector: 'app-customer',
@@ -16,36 +20,35 @@ import { catchError, finalize, tap } from 'rxjs/operators';
 })
 export class CustomerComponent implements OnInit {
   // Create Output Var From Model
-  customerData : customer ;
-  customerData_PK : customer_PK  = { "customerid" : '888'}; 
+  customerData: customer;
+  customerData_PK: customer_PK = { customerid: '888' };
 
-  resultShow:any ;
-  AresultShow:any = {};
+  resultShow: any;
+  AresultShow: any = {};
 
   //expressions = require("angular-expressions");
-  
 
   //customerData_PK.customerid = '999' ;
-  customerid : string = '1' ; 
-  isLoading :any ;
+  customerid: string = '1';
+  isLoading: any;
 
   objForm = {
     firstName: [''],
     lastName: [''],
-  } 
+  };
 
   objdepartmentForm = {
-    departmentCode: new FormControl('',[Validators.required]),
-    departmentDesc: new FormControl('',[Validators.required]),
+    departmentCode: new FormControl('', [Validators.required]),
+    departmentDesc: new FormControl('', [Validators.required]),
     faIcon: new FormControl(''),
     imageName: new FormControl(''),
     lang: new FormControl(''),
     lastupdate: new FormControl(''),
     salary: new FormControl(''),
     updatedby: new FormControl(''),
-  } 
+  };
 
-  // form_ItemMaster = { 
+  // form_ItemMaster = {
   //   Balance : ['' ],
   //   BalanceStock : ['' ],
   //   BrandID : ['' ],
@@ -81,119 +84,78 @@ export class CustomerComponent implements OnInit {
   //   ShopCode : ['' ,[Validators.required]],
   //   subImageURL : ['' ],
   //   }
-       
 
-  jobForm = this.fb.group( {
-    Balance : new FormControl(null ),
-    BalanceStock : new FormControl(null ),
-    BrandID : new FormControl(null ),
-    categoryCode : new FormControl(null ,[Validators.required]),
-    departmentCode : new FormControl(null ,[Validators.required]),
-    Description : new FormControl(null ),
-    Description2 : new FormControl(null ),
-    discount : new FormControl(null ),
-    fullPrice : new FormControl(null ),
-    groupcode : new FormControl(null ),
-    ImageListBig : new FormControl(null ),
-    ImageListThumbA : new FormControl(null ),
-    ImageListThumbB : new FormControl(null ),
-    isWork : new FormControl(null ,[Validators.required]),
-    ItemCode : new FormControl(null ,[Validators.required]),
-    ItemName : new FormControl(null ),
-    ItemNameShort : new FormControl(null ),
-    ItemNameURL : new FormControl(null ),
-    ItemShopCode : new FormControl(null ),
-    ItemShopURL : new FormControl(null ,[Validators.required]),
-    lastupdate : new FormControl(null ),
-    mainImageURL : new FormControl(null ),
-    numReview : new FormControl(null ),
-    NumSale : new FormControl(null ),
-    numView : new FormControl(null ),
-    pageDetailURL : new FormControl(null ),
-    pageMetaDesc : new FormControl(null ),
-    pageTitle : new FormControl(null ),
-    rating : new FormControl(null ),
-    SellPrice : new FormControl(null ),
-    shipCode : new FormControl(null ),
-    shipFeePerPiece : new FormControl(null ),
-    ShopCode : new FormControl(null ,[Validators.required]),
-    subImageURL : new FormControl(null ),
-  }
-  );
+  jobForm = this.fb.group({
+    categoryCode: new FormControl('', [Validators.required]),
+  });
 
   preview: string = '';
-  constructor(private fb: FormBuilder,private myservice:MyHttpService) {}
+  constructor(private fb: FormBuilder, private myservice: MyHttpService) {}
 
   ngOnInit() {
-   this.customerData_PK.customerid = '777' ;    
+    this.customerData_PK.customerid = '777';
 
-   this.jobForm.statusChanges.subscribe(x => {
-    alert(x);
-  })
-
-
+    this.jobForm.statusChanges.subscribe((x) => {
+      //alert(x);
+    });
   }
 
-  
-
-  async getDataA(tablename:string) {
-    this.AresultShow = {} ;
+  async getDataA(tablename: string) {
+    this.AresultShow = {};
     this.AresultShow = await this.myservice.getDatasUniverSal(tablename);
     console.log('DataLoad By Init1', this.AresultShow);
   }
 
-//  getDataDepartment900() {
-//    this.myservice.getAllDepartment('department')
-//    .pipe(
-//     finalize(() => {
-//       this.isLoading = false;
-//       alert('Yes') ;
-//     })
-//    )
-//    .subscribe({
-//     next: (data) => {
-//       this.AresultShow = data;
-//     },
-//     error: (e) => {      
-//       alert('Error while loading the product data');
-//     } ,
-//     complete: () => console.log('done')
-//   })
-//  } 
+  //  getDataDepartment900() {
+  //    this.myservice.getAllDepartment('department')
+  //    .pipe(
+  //     finalize(() => {
+  //       this.isLoading = false;
+  //       alert('Yes') ;
+  //     })
+  //    )
+  //    .subscribe({
+  //     next: (data) => {
+  //       this.AresultShow = data;
+  //     },
+  //     error: (e) => {
+  //       alert('Error while loading the product data');
+  //     } ,
+  //     complete: () => console.log('done')
+  //   })
+  //  }
 
-//  getDataDepartment999() {
-//   this.myservice.getAllDepartment('department')   
-//   .subscribe({
-//    next: (data) => {
-//      this.AresultShow = data;
-//    },
-//    error: (e) => {      
-//      alert('Error while loading the product data');
-//    } ,
-//    complete: () => console.log('done')
-//  })
-// } 
+  //  getDataDepartment999() {
+  //   this.myservice.getAllDepartment('department')
+  //   .subscribe({
+  //    next: (data) => {
+  //      this.AresultShow = data;
+  //    },
+  //    error: (e) => {
+  //      alert('Error while loading the product data');
+  //    } ,
+  //    complete: () => console.log('done')
+  //  })
+  // }
 
   onSubmit() {
-
-
+    alert('Save');
   }
   save() {
-    console.log('Save',JSON.stringify(this.jobForm.value)) ;
+    console.log('Save', JSON.stringify(this.jobForm.value));
     this.preview = JSON.stringify(this.jobForm.value);
   }
 
   SetPK() {
-    this.customerData_PK.customerid = '111777' ;    
-   // this.PKChange2(this.customerData_PK.customerid);
-   
+    this.customerData_PK.customerid = '111777';
+    // this.PKChange2(this.customerData_PK.customerid);
   }
 
   PKChange2(sValue) {
-    alert(sValue)
+    alert(sValue);
   }
 
-  PKChange(e:any) {
-    alert(e.target.value)
+  PKChange(e: any) {
+    alert(e.target.value);
   }
 }
