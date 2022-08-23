0481601a6b559571909324743827a1f9782edff0
nutv99 <nutv99@gmail.com>
2022-08-23 18:02:13 +0700

    2 files modified

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup , Validators  } from '@angular/forms';
+import {AbstractControl } from '@angular/forms';
 import { MyHttpService } from '../pick.service';
 import {customer,customer_PK} from '../model' ;
 
@@ -42,9 +43,47 @@ export class CustomerComponent implements OnInit {
     lastupdate: new FormControl(''),
     salary: new FormControl(''),
     updatedby: new FormControl(''),
-  }
+  } 
 
-  jobForm = this.fb.group(this.objForm);
+  form_ItemMaster = { 
+    Balance : new FormControl('' ),
+    BalanceStock : new FormControl('' ),
+    BrandID : new FormControl('' ),
+    categoryCode : new FormControl('' ,[Validators.required]),
+    departmentCode : new FormControl('' ,[Validators.required]),
+    Description : new FormControl('' ),
+    Description2 : new FormControl('' ),
+    discount : new FormControl('' ),
+    fullPrice : new FormControl('' ),
+    groupcode : new FormControl('' ),
+    ImageListBig : new FormControl('' ),
+    ImageListThumbA : new FormControl('' ),
+    ImageListThumbB : new FormControl('' ),
+    isWork : new FormControl('' ,[Validators.required]),
+    ItemCode : new FormControl('' ,[Validators.required]),
+    ItemName : new FormControl('' ),
+    ItemNameShort : new FormControl('' ),
+    ItemNameURL : new FormControl('' ),
+    ItemShopCode : new FormControl('' ),
+    ItemShopURL : new FormControl('' ,[Validators.required]),
+    lastupdate : new FormControl('' ),
+    mainImageURL : new FormControl('' ),
+    numReview : new FormControl('' ),
+    NumSale : new FormControl('' ),
+    numView : new FormControl('' ),
+    pageDetailURL : new FormControl('' ),
+    pageMetaDesc : new FormControl('' ),
+    pageTitle : new FormControl('' ),
+    rating : new FormControl('' ),
+    SellPrice : new FormControl('' ),
+    shipCode : new FormControl('' ),
+    shipFeePerPiece : new FormControl('' ),
+    ShopCode : new FormControl('' ,[Validators.required]),
+    subImageURL : new FormControl('' )
+  } 
+  
+
+  jobForm = this.fb.group(this.form_ItemMaster);
 
   preview: string = '';
   constructor(private fb: FormBuilder,private myservice:MyHttpService) {}
