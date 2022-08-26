a8d2713d7e2a2f8c615ed7e2c54213124a208a79
nutv99 <nutv99@gmail.com>
2022-08-26 07:43:53 +0700

    3 files modified

--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { PkDirective } from '../pk-directive.directive';
 import { DiRect2Directive } from '../di-rect2.directive';
+import { of } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 interface City {
   name: string;
@@ -12,19 +14,27 @@ interface City {
   templateUrl: './contactus.component.html',
   styleUrls: ['./contactus.component.css'],
 })
-
-export class ContactusComponent implements  AfterViewInit {
+export class ContactusComponent implements AfterViewInit {
   resultShow: City[];
   resultShow2: City[];
   department: City[];
   sText: string;
   sText2: string;
+  isLoading: boolean = false;
+
+  // const series1$ = of('a', 'b');
 
   constructor() {}
 
   ngOnInit() {
-    
-  } 
+    // $.getScript('../js/myjsfile.js');
+    let d = of(1, 2, 3, 4).pipe(
+      filter((x) => x % 2 === 0),
+      map((x) => x + x)
+    );
+
+    d.subscribe((x) => console.log('x-rx=', x));
+  }
   ngAfterViewInit() {
     let element: HTMLElement = document.getElementById('mText2') as HTMLElement;
     element.click();
@@ -32,5 +42,18 @@ export class ContactusComponent implements  AfterViewInit {
 
   setText() {
     this.sText = 'ttt';
+  }
+
+  StopWating() {
+    this.isLoading = false;    
+  }
+
+  setisLoading() {
+    this.isLoading = true;
+    // const myTimeout = setTimeout(this.StopWating, 2000);
+    setTimeout(()=>{                           //<<<---using ()=> syntax
+      this.isLoading = false;
+   }, 3000);
+    
   }
 }
